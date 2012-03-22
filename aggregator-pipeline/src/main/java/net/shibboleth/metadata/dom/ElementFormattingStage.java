d6cea708cc2cde62a60baee483d9b62445d318de
Chad La Joie <clajoie@gmail.com>
2012-03-22 13:35:50 +0000

    Initial, untested, impl of a formatting (e.g. pretty-printing) stage

---
+++ aggregator-pipeline/src/main/java/net/shibboleth/metadata/dom/ElementFormattingStage.java
@@ -0,0 +1,201 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.metadata.dom;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+
+import net.shibboleth.metadata.pipeline.BaseStage;
+import net.shibboleth.metadata.pipeline.StageProcessingException;
+import net.shibboleth.metadata.util.ItemMetadataSupport;
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Assert;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
+
+import org.w3c.dom.Element;
+
+import com.google.common.collect.Lists;
+
+/**
+ * A stage that formats a collection of DOM Elements.
+ * 
+ * <p>
+ * Note, this stage uses Xalan specific options and so Xalan must be included on the classpath. However, Xalan does
+ * <strong>not</strong> have to be the system default {@link TransformerFactory} implementation.
+ * </p>
+ */
+public class ElementFormattingStage extends BaseStage<DomElementItem> {
+
+    /** Line separator character to use. Default value: \n */
+    private String lineSeperator = "\n";
+
+    /** Whether to indent elements. Default value: true */
+    private boolean indented = true;
+
+    /** Number of spaces used to indent elements. Default value: 4 */
+    private int indentSize = 4;
+
+    /**
+     * List of elements whose content should be wrapped in CDATA sections. Elements are specified either by their local
+     * name, if they are not in a namespace, or via the form '{' + namespace URI + '}' + local name if they are in a
+     * namespace.
+     */
+    private List<String> cdataSectionElements;
+
+    /** The factory used to create the {@link Transformer} used to format the elements. */
+    private TransformerFactory transformerFactory;
+
+    /**
+     * Gets the line separator character to use.
+     * 
+     * @return line separator character to use
+     */
+    @Nonnull public String getLineSeperator() {
+        return lineSeperator;
+    }
+
+    /**
+     * Sets the line separator character to use.
+     * 
+     * @param seperator line separator character to use
+     */
+    public synchronized void setLineSeperator(@Nonnull @NotEmpty final String seperator) {
+        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        lineSeperator = Assert.isNotNull(StringSupport.trimOrNull(seperator), "Line seperator can not be null");
+    }
+
+    /**
+     * Gets whether to indent elements.
+     * 
+     * @return whether to indent elements
+     */
+    public boolean isIndented() {
+        return indented;
+    }
+
+    /**
+     * Sets whether to indent elements.
+     * 
+     * @param indentElements whether to indent elements
+     */
+    public synchronized void setIndented(boolean indentElements) {
+        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        indented = indentElements;
+    }
+
+    /**
+     * Gets the number of spaces to use when indenting elements.
+     * 
+     * @return number of spaces to use when indenting elements
+     */
+    public int getIndentSize() {
+        return indentSize;
+    }
+
+    /**
+     * Sets the number of spaces to use when indenting elements.
+     * 
+     * @param size number of spaces to use when indenting elements, must be 0 or greater
+     */
+    public synchronized void setIndentSize(int size) {
+        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        indentSize = (int) Assert.isGreaterThanOrEqual(0, size, "Indentation size must be 0 or greater");
+    }
+
+    /** {@inheritDoc} */
+    protected void doExecute(Collection<DomElementItem> itemCollection) throws StageProcessingException {
+        ArrayList<DomElementItem> transformedItems = Lists.newArrayListWithExpectedSize(itemCollection.size());
+
+        DOMSource source;
+        DOMResult result;
+        DomElementItem transformedItem;
+        for (DomElementItem item : itemCollection) {
+            source = new DOMSource(item.unwrap());
+            result = new DOMResult();
+
+            try {
+                getTransformer().transform(source, result);
+                transformedItem = new DomElementItem((Element) result.getNode());
+                ItemMetadataSupport.addAll(transformedItem, item.getItemMetadata().values());
+                transformedItems.add(transformedItem);
+            } catch (TransformerException e) {
+                throw new StageProcessingException("Unable to format Element", e);
+            }
+        }
+
+        itemCollection.clear();
+        itemCollection.addAll(transformedItems);
+    }
+
+    /**
+     * Builds the {@link Transformer} that will be used to format the element.
+     * 
+     * @return the {@link Transformer} that will be used to format the element
+     * 
+     * @throws StageProcessingException thrown if the configuration of the {@link TransformerFactory} has become
+     *             corrupted since stage initialization
+     */
+    protected Transformer getTransformer() throws StageProcessingException {
+        try {
+            Transformer transformer = transformerFactory.newTransformer();
+
+            transformer.setOutputProperty("{http://xml.apache.org/xalan}line-separator", lineSeperator);
+
+            if (indented) {
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount",
+                        Integer.toString(indentSize));
+            } else {
+                transformer.setOutputProperty(OutputKeys.INDENT, "no");
+            }
+
+            transformer.setOutputProperty(OutputKeys.CDATA_SECTION_ELEMENTS,
+                    StringSupport.listToStringValue(cdataSectionElements, ","));
+
+            return transformer;
+        } catch (TransformerConfigurationException e) {
+            // in theory this shouldn't be possible as we've already checked the config during initialization
+            // and nothing else can change the factory, but just in case...
+            throw new StageProcessingException("Unable to create a new Transformer", e);
+        }
+    }
+
+    /** {@inheritDoc} */
+    protected void doInitialize() throws ComponentInitializationException {
+        super.doInitialize();
+
+        transformerFactory =
+                TransformerFactory.newInstance("org.apache.xalan.processor.TransformerFactoryImpl", getClass()
+                        .getClassLoader());
+    }
+}
