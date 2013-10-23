dfbc3f0d7585fa9eaf99f63e1db27c45f615b118
Ian Young <ian@iay.org.uk>
2013-10-23 11:21:20 +0000

    MDA-121: add stage to extract mdrpi registrationAuthority value into item metadata

---
+++ aggregator-pipeline/src/main/java/net/shibboleth/metadata/dom/saml/mdrpi/RegistrationAuthorityPopulationStage.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.metadata.dom.saml.mdrpi;
+
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
+
+import net.shibboleth.metadata.ErrorStatus;
+import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.ItemMetadata;
+import net.shibboleth.metadata.dom.saml.SAMLMetadataSupport;
+import net.shibboleth.metadata.pipeline.BaseStage;
+import net.shibboleth.metadata.pipeline.StageProcessingException;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+import net.shibboleth.utilities.java.support.collection.ClassToInstanceMultiMap;
+import net.shibboleth.utilities.java.support.xml.AttributeSupport;
+
+import org.w3c.dom.Element;
+
+import com.google.common.base.Strings;
+
+/**
+ * A stage which, for each EntityDescriptor collection element, adds a {@link RegistrationAuthority}, with
+ * the entity's registration authority, to the item metadata.
+ */
+@ThreadSafe
+public class RegistrationAuthorityPopulationStage extends BaseStage<Element> {
+
+    /** {@inheritDoc} */
+    protected void doExecute(@Nonnull @NonnullElements final Collection<Item<Element>> items)
+            throws StageProcessingException {
+
+        for (Item<Element> item : items) {
+           final Element entity = item.unwrap();
+           final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+           
+           if (!SAMLMetadataSupport.isEntityDescriptor(entity)) {
+               // all items must be EntityDescriptor elements
+               metadata.put(new ErrorStatus(getId(), "item was not an EntityDescriptor"));
+           } else {
+               // Extract mdrpi:RegistrationInfo if present.
+               final Element regInfo = SAMLMetadataSupport.getDescriptorExtensions(entity,
+                       MDRPISupport.MDRPI_REGISTRATION_INFO);
+               if (regInfo != null) {
+                   // Extract registrationAuthority
+                   final String attr = AttributeSupport.getAttributeValue(regInfo, null, "registrationAuthority");
+                   if (attr == null) {
+                       final String eid = Strings.nullToEmpty(
+                               AttributeSupport.getAttributeValue(entity, null, "entityID"));
+                       metadata.put(new ErrorStatus(getId(), "RegistrationInfo for " + eid +
+                               " did not have a RegistrationAuthority attribute"));
+                   } else {
+                       metadata.put(new RegistrationAuthority(attr));
+                   }
+               }
+           }
+           
+        }
+    }
+    
+}
---
+++ aggregator-pipeline/src/test/java/net/shibboleth/metadata/dom/saml/mdrpi/RegistrationAuthorityPopulationStageTest.java
@@ -0,0 +1,101 @@
+
+package net.shibboleth.metadata.dom.saml.mdrpi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.shibboleth.metadata.ErrorStatus;
+import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.ItemMetadata;
+import net.shibboleth.metadata.dom.BaseDOMTest;
+import net.shibboleth.metadata.dom.DOMElementItem;
+import net.shibboleth.metadata.dom.saml.mdrpi.RegistrationAuthority;
+import net.shibboleth.metadata.dom.saml.mdrpi.RegistrationAuthorityPopulationStage;
+import net.shibboleth.utilities.java.support.collection.ClassToInstanceMultiMap;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.xml.XMLParserException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.w3c.dom.Element;
+
+public class RegistrationAuthorityPopulationStageTest extends BaseDOMTest {
+
+    @BeforeClass
+    private void init() {
+        setTestingClass(RegistrationAuthorityPopulationStage.class);
+    }
+
+    private RegistrationAuthorityPopulationStage makeStage() throws ComponentInitializationException {
+        final RegistrationAuthorityPopulationStage stage = new RegistrationAuthorityPopulationStage();
+        stage.setId("test");
+        stage.initialize();
+        return stage; 
+    }
+    
+    private DOMElementItem makeItem(final String which) throws XMLParserException {
+        final Element doc = readXmlData(classRelativeResource(which + ".xml"));
+        return new DOMElementItem(doc);
+    }
+    
+    @Test
+    public void populatePresent() throws Exception {
+        final DOMElementItem item = makeItem("present");
+        
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final RegistrationAuthorityPopulationStage stage = makeStage();
+        
+        stage.execute(items);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        final List<RegistrationAuthority> regAuths = metadata.get(RegistrationAuthority.class);
+        Assert.assertEquals(regAuths.size(), 1);
+        final RegistrationAuthority regAuth = regAuths.get(0);
+        Assert.assertEquals(regAuth.getRegistrationAuthority(), "http://ukfederation.org.uk");
+    }
+    
+    @Test
+    public void populateAbsent() throws Exception  {
+        final DOMElementItem item = makeItem("absent");
+        
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final RegistrationAuthorityPopulationStage stage = makeStage();
+        
+        stage.execute(items);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        final List<RegistrationAuthority> regAuths = metadata.get(RegistrationAuthority.class);
+        Assert.assertEquals(regAuths.size(), 0);
+    }
+    
+    @Test
+    public void populateNoExtensions() throws Exception  {
+        final DOMElementItem item = makeItem("noext");
+        
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final RegistrationAuthorityPopulationStage stage = makeStage();
+        
+        stage.execute(items);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        final List<RegistrationAuthority> regAuths = metadata.get(RegistrationAuthority.class);
+        Assert.assertEquals(regAuths.size(), 0);
+    }
+    
+}
