ab2b46038f59d4c8e0b61ad060396e81745fe7ca
Ian Young <ian@iay.org.uk>
2020-05-25 14:38:59 +0100

    MDA-138 add SAMLStringElementCheckingStage
    https://issues.shibboleth.net/jira/browse/MDA-138

---
+++ aggregator-pipeline/src/main/java/net/shibboleth/metadata/dom/saml/SAMLStringElementCheckingStage.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.metadata.dom.saml;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.metadata.dom.AbstractElementVisitingStage;
+import net.shibboleth.metadata.dom.DOMTraversalContext;
+import net.shibboleth.utilities.java.support.xml.QNameSupport;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+/**
+ * A Stage which checks the text content of the named elements to verify that
+ * they meet the constraints of SAML string values.
+ */
+public class SAMLStringElementCheckingStage extends AbstractElementVisitingStage {
+
+    /** Regular expression matching a string which contains no non-whitespace characters. */
+    private static final Pattern ALL_WHITE_SPACE_PATTERN = Pattern.compile("^[ \\t\\r\\n\\x85\\u2028]*$");
+
+    /**
+     * Returns the {@link Element} representing the EntityDescriptor which is the
+     * closest-containing ancestor of the given element.
+     * 
+     * @param element {@link Element} to locate the ancestor Entity of.
+     * @return ancestor EntityDescriptor {@link Element}, or null.
+     */
+    private Element ancestorEntity(@Nonnull final Element element) {
+        assert element != null;
+        for (Element e = element; e != null; e = (Element) e.getParentNode()) {
+            if (SAMLMetadataSupport.isEntityDescriptor(e)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Check that the string value is appropriate.
+     * 
+     * This is done using a regular expression because Java's {@link String#trim} method does not
+     * use XML's definition of white space.
+     * 
+     * @param visited DOM {@link Node} being checked
+     * @return <code>true</code> if the {@link Node}'s value matches
+     */
+    private static boolean match(@Nonnull final Node visited) {
+        assert visited != null;
+        final String textContent = visited.getTextContent();
+        final Matcher matcher = ALL_WHITE_SPACE_PATTERN.matcher(textContent);
+        return matcher.matches();
+    }
+
+    @Override
+    protected void visit(@Nonnull final Element e, @Nonnull final DOMTraversalContext context) {
+        if (match(e)) {
+            final StringBuilder b = new StringBuilder("element ");
+            b.append(QNameSupport.getNodeQName(e));
+            b.append(" must contain at least one non-whitespace character");
+            final Element entity = ancestorEntity(e);
+            addError(context.getItem(), entity, b.toString());
+        }
+    }
+
+}
---
+++ aggregator-pipeline/src/test/java/net/shibboleth/metadata/dom/saml/SAMLStringElementCheckingStageTest.java
@@ -0,0 +1,65 @@
+
+package net.shibboleth.metadata.dom.saml;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.w3c.dom.Element;
+
+import net.shibboleth.metadata.ErrorStatus;
+import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.dom.BaseDOMTest;
+import net.shibboleth.metadata.dom.DOMElementItem;
+
+public class SAMLStringElementCheckingStageTest extends BaseDOMTest {
+
+    /** Constructor sets class under test. */
+    public SAMLStringElementCheckingStageTest() {
+        super(SAMLStringElementCheckingStage.class);
+    }
+    
+    @Test
+    public void testOK() throws Exception {
+        final Item<Element> item = new DOMElementItem(readXMLData("ok.xml"));
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final SAMLStringElementCheckingStage stage = new SAMLStringElementCheckingStage();
+        stage.setId("test");
+        stage.setElementNames(Set.of(SAMLMetadataSupport.ORGANIZATIONNAME_NAME));
+        stage.initialize();
+        
+        stage.execute(items);
+        
+        final Item<Element> outItem = items.get(0);
+        Assert.assertSame(outItem, item);
+        final List<ErrorStatus> errors = item.getItemMetadata().get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+    }
+    
+    @Test
+    public void testFail() throws Exception {
+        final Item<Element> item = new DOMElementItem(readXMLData("fail.xml"));
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final SAMLStringElementCheckingStage stage = new SAMLStringElementCheckingStage();
+        stage.setId("test");
+        stage.setElementNames(Set.of(SAMLMetadataSupport.ORGANIZATIONNAME_NAME,
+                SAMLMetadataSupport.ORGANIZATIONDISPLAYNAME_NAME));
+        stage.initialize();
+        
+        stage.execute(items);
+        
+        final Item<Element> outItem = items.get(0);
+        Assert.assertSame(outItem, item);
+        final List<ErrorStatus> errors = item.getItemMetadata().get(ErrorStatus.class);
+        for (final ErrorStatus error : errors) {
+            System.out.println(error.getComponentId() + ": " + error.getStatusMessage());
+        }
+        Assert.assertEquals(errors.size(), 3);
+    }
+}
