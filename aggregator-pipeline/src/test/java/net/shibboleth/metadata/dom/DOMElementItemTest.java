3c4c6f554d0f643a4500c5edd99f2108aa871b13
Ian Young <ian@iay.org.uk>
2020-11-05 18:00:00 +0000

    MDA-65 - Catch up on unit tests
    https://issues.shibboleth.net/jira/browse/MDA-65

--- aggregator-pipeline/src/test/java/net/shibboleth/metadata/dom/DOMElementItemTest.java
+++ aggregator-pipeline/src/test/java/net/shibboleth/metadata/dom/DOMElementItemTest.java
@@ -1,13 +1,20 @@
 
 package net.shibboleth.metadata.dom;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.ItemId;
 import net.shibboleth.utilities.java.support.logic.ConstraintViolationException;
 
-public class DOMElementItemTest {
+public class DOMElementItemTest extends BaseDOMTest {
+
+    protected DOMElementItemTest() {
+        super(DOMElementItem.class);
+    }
 
     @Test(expectedExceptions=ConstraintViolationException.class)
     public void testNullDocument() {
@@ -17,5 +24,31 @@ public class DOMElementItemTest {
     @Test(expectedExceptions=ConstraintViolationException.class)
     public void testNullElement() {
         new DOMElementItem((Element)null);
+    }
+    
+    @Test
+    public void testCopy() throws Exception {
+        // Build an item with some metadata
+        final var e = readXMLData("in.xml");
+        final Item<Element> orig = new DOMElementItem(e);
+        final var origId = new ItemId("item");
+        orig.getItemMetadata().put(origId);
+
+        // Make a copy
+        final Item<Element> copy = orig.copy();
+
+        // The original and copy should be different, their metadata collections
+        // different, the actual metadata the same.
+        final var copyId = copy.getItemMetadata().get(ItemId.class).get(0);
+        Assert.assertNotNull(copyId);
+        Assert.assertSame(copyId, origId);
+        Assert.assertNotSame(copy, orig);
+        Assert.assertNotSame(copy.getItemMetadata(), orig.getItemMetadata());
+        
+        // Values should be identical
+        assertXMLIdentical(copy.unwrap(), orig.unwrap());
+        
+        // Values should be in different documents.
+        Assert.assertNotSame(copy.unwrap().getOwnerDocument(), orig.unwrap().getOwnerDocument());
     }
 }
