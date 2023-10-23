fcfdcf373538adbe64c679bcc953528dd15906df
Ian Young <ian@iay.org.uk>
2023-10-23 17:30:20 +0100

    MDA-297 - Add a softhsm token
    https://shibboleth.atlassian.net/browse/MDA-297

--- doc/wiki/make-keys.sh
+++ doc/wiki/make-keys.sh
@@ -2,6 +2,7 @@
 
 KEYFILE=path/to/input/private-key.pem
 CERTFILE=path/to/input/self-signed.pem
+P12FILE=path/to/input/self-signed.p12
 
 # Generate an RSA private key
 openssl genrsa >$KEYFILE
@@ -10,3 +11,6 @@ chmod 600 $KEYFILE
 # Generate a self-signed certificate based on that key
 openssl req -key $KEYFILE -new -x509 -days 365 -out $CERTFILE \
     -subj "/CN=test-self-signed"
+
+# Create PKCS12 keystore from private key and public certificate.
+openssl pkcs12 -export -name key10 -passout pass:password -in $CERTFILE -inkey $KEYFILE -out $P12FILE
