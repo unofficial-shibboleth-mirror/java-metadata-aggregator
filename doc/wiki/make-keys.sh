6c0e3ae6989173902a866915e92b7ba4b53837d4
Ian Young <ian@iay.org.uk>
2023-10-23 14:46:45 +0100

    MDA-297 - Add a self-signed certificate
    https://shibboleth.atlassian.net/browse/MDA-297

--- doc/wiki/make-keys.sh
+++ doc/wiki/make-keys.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/sh
 
-openssl genrsa >path/to/input/private-key.pem
+KEYFILE=path/to/input/private-key.pem
+CERTFILE=path/to/input/self-signed.pem
+
+# Generate an RSA private key
+openssl genrsa >$KEYFILE
+chmod 600 $KEYFILE
+
+# Generate a self-signed certificate based on that key
+openssl req -key $KEYFILE -new -x509 -days 365 -out $CERTFILE \
+    -subj "/CN=test-self-signed"
